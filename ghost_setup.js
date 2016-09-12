e55d2f7a0eee9b95e7720d7e92374dab20714111
Keith Garrod <keith@coachwidgets.com>
2016-09-12 14:21:08 +0200

    add comment

--- ghost_setup.js
+++ ghost_setup.js
@@ -1,6 +1,6 @@
 (function () {
     var ghost = require('ghost'), path = require('path'), config = require(path.join(__dirname, 'ghost_config.js'));
-    console.log('config', JSON.stringify(config));
+    console.log('config', JSON.stringify(config), process.env.NODE_ENV);
     ghost({
         config: path.join(__dirname, 'ghost_config.js')
     }).then(function (ghostServer) {

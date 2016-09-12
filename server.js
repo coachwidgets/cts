ba12f08edfec4b71b90500b08db01501edd8ba7e
Keith Garrod <keith@coachwidgets.com>
2016-09-12 07:03:14 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require('express'), ejs = require('ejs'), path = require('path'), root = path.join(__dirname, 'build/unbundled'), favicon = require('serve-favicon'), logger = require('morgan'), bodyParser = require('body-parser'), ghost = require('ghost'), app = express();
+ghost().then(function (ghostServer) {
+    app.use(ghostServer.config.paths.subdir, ghostServer.rootApp);
+    ghostServer.start(app);
+    app.set('port', (process.env.PORT || 5000))
+        .set('views', root)
+        .engine('html', ejs.renderFile)
+        .use(logger('dev'))
+        .use(bodyParser.json())
+        .use(bodyParser.urlencoded({ extended: false }))
+        .use(express.static(root))
+        .use(function (req, res, next) {
+        var err = {
+            message: 'Not Found',
+            status: 404
+        };
+        next(err);
+    });
+    if (process.env === 'develop') {
+        app.use(function (err, req, res, next) {
+            res.status(err.status || 500);
+            res.render('error', {
+                message: err.message,
+                error: err
+            });
+        });
+    }
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {}
+        });
+    })
+        .get('/', function (req, res) {
+        res.render('index.html');
+    })
+        .listen(app.get('port'));
+});
---
+++ server.ts
@@ -0,0 +1,63 @@
+let express = require('express'),
+    ejs = require('ejs'),
+    path = require('path'),
+    root = path.join(__dirname, 'build/unbundled'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    bodyParser = require('body-parser'),
+    ghost = require('ghost'),
+    app = express();
+
+ghost().then((ghostServer) => {
+    app.use(ghostServer.config.paths.subdir, ghostServer.rootApp);
+    ghostServer.start(app);
+
+    app.set('port', (process.env.PORT || 5000))
+        // view engine setup
+        .set('views', root)
+        .engine('html', ejs.renderFile)
+
+        // uncomment after placing your favicon in /public
+        //.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+        .use(logger('dev'))
+        .use(bodyParser.json())
+        .use(bodyParser.urlencoded({ extended: false }))
+        .use(express.static(root))
+
+        // catch 404 and forward to error handler
+        .use((req, res, next) => {
+            let err = {
+                message: 'Not Found',
+                status: 404
+            }
+            next(err);
+        });
+
+    // error handlers
+
+    // development error handler
+    // will print stacktrace
+    if (process.env === 'develop') {
+        app.use((err, req, res, next) => {
+            res.status(err.status || 500);
+            res.render('error', {
+                message: err.message,
+                error: err
+            });
+        });
+    }
+
+    // production error handler
+    // no stacktraces leaked to user
+    app.use((err, req, res, next) => {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {}
+        });
+    })
+        .get('/', (req, res) => {
+            res.render('index.html');
+        })
+        .listen(app.get('port'));
+})

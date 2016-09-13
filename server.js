d04e8623feb8b06a2956d383bb4b22f49abdcc23
Keith Garrod <keith@coachwidgets.com>
2016-09-13 13:10:25 +0200

    just ghost

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-var express = require('express'), ejs = require('ejs'), path = require('path'), configPath = path.join(__dirname, 'ghost/config.js'), ghostConfig = require(configPath)[process.env.NODE_ENV], root = path.join(__dirname, 'build/unbundled'), favicon = require('serve-favicon'), logger = require('morgan'), ghost = require('ghost'), app = express();
-console.log("path.join(__dirname, 'ghost/config.js'), ghostConfig", path.join(__dirname, 'ghost/config.js'), ghostConfig);
+var express = require('express'), ejs = require('ejs'), path = require('path'), ghostConfigPath = path.join(__dirname, 'ghost/config.js'), root = path.join(__dirname, 'build/unbundled'), favicon = require('serve-favicon'), logger = require('morgan'), ghost = require('ghost'), app = express();
 ghost({
-    config: path.join(__dirname, 'ghost/config.js')
+    config: ghostConfigPath
 }).then(function (ghostServer) {
     console.log('ghostServer.config.paths.subdir, ghostServer.rootApp', ghostServer.config.paths.subdir, ghostServer.rootApp);
-    app.use(ghostServer.config.paths.subdir, ghostServer.rootApp);
-    app.set('views', root)
+    app.use(ghostServer.config.paths.subdir, ghostServer.rootApp)
+        .set('views', root)
         .engine('html', ejs.renderFile)
         .use(logger('dev'))
         .use(express.static(root))
-        .get('/a', function (req, res) {
+        .get('/app', function (req, res) {
         res.render('index.html');
     });
-    ghostServer.start(app);
-    console.log('After Ghost then', process.env.port);
+    ghostServer.start();
 })
     .catch(function (err) {
     console.log('Ghost failed', err);
--- server.ts
+++ server.ts
@@ -1,35 +1,29 @@
 let express = require('express'),
     ejs = require('ejs'),
     path = require('path'),
-    configPath = path.join(__dirname, 'ghost/config.js'),
-    ghostConfig = require(configPath)[process.env.NODE_ENV],
+    ghostConfigPath = path.join(__dirname, 'ghost/config.js'),
     root = path.join(__dirname, 'build/unbundled'),
     favicon = require('serve-favicon'),
     logger = require('morgan'),
     ghost = require('ghost'),
     app = express();
-console.log("path.join(__dirname, 'ghost/config.js'), ghostConfig", path.join(__dirname, 'ghost/config.js'), ghostConfig)
 
 ghost({
-    config: path.join(__dirname, 'ghost/config.js')
+    config: ghostConfigPath
 }).then((ghostServer) => {
     console.log('ghostServer.config.paths.subdir, ghostServer.rootApp', ghostServer.config.paths.subdir, ghostServer.rootApp)
-    app.use(ghostServer.config.paths.subdir, ghostServer.rootApp);
-    // view engine setup
-    app.set('views', root)
+    app.use(ghostServer.config.paths.subdir, ghostServer.rootApp)
+    .set('views', root)
     .engine('html', ejs.renderFile)
 
     // uncomment after placing your favicon in /public
     //.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
     .use(logger('dev'))
     .use(express.static(root))
-    .get('/a', (req, res) => {
+    .get('/app', (req, res) => {
       res.render('index.html');
     })
-    ghostServer.start(app);
-    console.log('After Ghost then', process.env.port)
-        //.listen(process.env.PORT || 5000);
-
+    ghostServer.start();
 })
     .catch((err) => {
         console.log('Ghost failed', err)
